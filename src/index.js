602095dd7527872eb07e93617640b7850e4d00b4
Tulio <tuliotrindade99@gmail.com>
2021-12-06 14:00:17 -0300

    corrigindo conecção do db no heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cryptoRouter = require('./routes/cryptoRoutes');
-require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
