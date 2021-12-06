63a9031c827a143d976c9daedd2a1473bbd6be70
Tulio <tuliotrindade99@gmail.com>
2021-12-06 13:37:58 -0300

    corrigindo erro heroku 2

--- src/db.js
+++ src/db.js
@@ -1,4 +1,12 @@
 const sequelize = require('sequelize');
-const connection = new sequelize('desafio-tecnico', 'postgres', 'senha123', { dialect: 'postgres'});
+
+const connection  =  new  sequelize ( process.env.DATABASE_URL ,  { 
+	dialect:  'postgres',
+	protocol: 'postgres',
+	logging:  true 
+} ) ; 
+/* const connection = new sequelize('desafio-tecnico', 'postgres', 'senha123', { dialect: 'postgres'}); */
+
+
 
 module.exports = connection;
