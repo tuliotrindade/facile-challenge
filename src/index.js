fd1fcb1d1421549cf9b963c32b9c03bcbb7c3910
Tulio <tuliotrindade99@gmail.com>
2021-12-06 13:23:15 -0300

    corrigindo erro heroku

--- src/index.js
+++ src/index.js
@@ -5,17 +5,6 @@ const cryptoRouter = require('./routes/cryptoRoutes');
 const app = express();
 app.use(bodyParser.json());
 
-(async () => {
-   const database = require('./db');
-
-   try {
-       const result = await database.sync();
-       console.log(result);
-   } catch (error) {
-       console.log(error);
-   }
-})();
-
 const PORT = process.env.PORT || 3333;
 
 app.use('/encripts', cryptoRouter);
@@ -23,3 +12,15 @@ app.use('/encripts', cryptoRouter);
 app.listen(PORT, () => {
   console.log(`Aplicação ouvindo na porta ${ PORT }`);
 });
+
+/* (async () => {
+    const database = require('./db');
+    const encryptedName = require('./models/encryptedName');
+ 
+    try {
+        const result = await database.sync();
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+})(); */
